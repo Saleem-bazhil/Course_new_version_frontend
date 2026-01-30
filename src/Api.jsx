96402fa6f07630c02adb2da187d53cd4c95ba85a
saleem <ab588538@gmail.com>
2026-01-30 10:53:56 +0530

    fix the api

--- src/Api.jsx
+++ src/Api.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "https://pdfweb-frontend.onrender.com/api",
-  baseURL: "http://localhost:5050/api",
+  baseURL: "https://www.skiezpdfbooks.in/api",
   withCredentials: false,
 });
 
